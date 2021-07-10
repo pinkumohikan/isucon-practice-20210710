15ded3a240ae259ea9f49f7391b251d0eb9d7648
Hokuto Shinoda <pinkumohikan+github@gmail.com>
2021-07-11 01:16:40 +0900

    fix: sleep 5秒はまだ耐えられそうだった

--- webapp/go/src/torb/app.go
+++ webapp/go/src/torb/app.go
@@ -1005,7 +1005,7 @@ func main() {
 	}, adminLoginRequired)
 	e.GET("/admin/api/reports/sales", func(c echo.Context) error {
 		// このエンドポイントは1req15MBぐらいあってすごい重いわりに1点なので、あんまり呼ばれないようtimeoutギリギリにしておく
-		time.Sleep(time.Second * 3)
+		time.Sleep(time.Second * 5)
 		rows, err := db.Query("select r.*, s.rank as sheet_rank, s.num as sheet_num, s.price as sheet_price, e.id as event_id, e.price as event_price from reservations r inner join sheets s on s.id = r.sheet_id inner join events e on e.id = r.event_id order by reserved_at asc")
 		if err != nil {
 			return err
